878382e2f29bfeb56164929eee3c2637a298a396
leandro <leandro_antunes28@hotmail.com>
2024-07-27 01:37:24 -0300

    removendo obrigacao min 6 caracteres na senha

--- src/models/validacaoAutenticacao.js
+++ src/models/validacaoAutenticacao.js
@@ -2,7 +2,7 @@ const yup = require('yup');
 
 const validacaoAutenticacao = yup.object().shape({
     email: yup.string("Email incorreto").email("Informe um email válido").required("o email é obrigatório"),
-    senha: yup.string("Informe uma senha válida").required("A senha é obrigatória").min(6, "A senha deve ter no mínimo 6 caracteres")
+    senha: yup.string("Informe uma senha válida").required("A senha é obrigatória")
 });
 
 module.exports = validacaoAutenticacao;
